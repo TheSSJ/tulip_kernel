9ef2d77992481cfb9a4fe3aa460f1a5445ab95d8
franciscofranco <franciscofranco.1990@gmail.com>
2017-11-20 12:58:55 +0000

    block: row: add magic values

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,5 +8,5 @@ make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE O=out msm8952-tulip_defconfig
 echo "Next step"
 make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE O=out -j5
 
-mv out/arch/arm64/boot/Image.gz-dtb ../AnyKernel-master/
+mv out/arch/arm64/boot/Image.gz-dtb ..
 
